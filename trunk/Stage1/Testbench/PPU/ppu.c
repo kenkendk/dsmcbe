add4cbaa48b69cea8181f40b0b6fe9c0d7b9594f
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-04-25 14:03:36 +0000

    New improved structure

---
+++ trunk/Stage1/Testbench/PPU/ppu.c
@@ -0,0 +1,41 @@
+#include <dsmcbe_ppu.h>
+#include <stdio.h>
+#include "../guids.h"
+#include <common/debug.h>
+
+#define SPU_THREADS 1
+
+int main(int argc, char **argv) {
+	
+	unsigned long size;
+
+	printf(WHERESTR "Starting\n", WHEREARG);
+
+	pthread_t* spu_threads;
+	spu_threads = simpleInitialize(SPU_THREADS);
+
+	/*printf("ppu.c: Going to sleep\n");
+	sleep(1);*/
+	
+	printf(WHERESTR "Creating\n", WHEREARG);
+	int* data = create(ETTAL, sizeof(int));
+	(*data) = 928;
+	
+	printf(WHERESTR "Data location is %i\n", WHEREARG, (unsigned int)data);
+	
+	printf(WHERESTR "Releasing\n", WHEREARG);
+	release(data);
+	
+	printf(WHERESTR "Released, waiting for SPU to complete\n", WHEREARG);
+	pthread_join(spu_threads[SPU_THREADS - 1], NULL);
+	
+	data = acquire(ETTAL, &size);
+	printf(WHERESTR "ppu.c: Data location is %i\n", WHEREARG, (unsigned int)data);
+	printf(WHERESTR "ppu.c: Value is %i\n", WHEREARG, *data);
+	
+	release(data);
+	
+	printf(WHERESTR "All done, exiting cleanly\n", WHEREARG);
+	
+	return 0;
+}
---
+++ trunk/Stage1/Testbench/SPU/dsmcbe_spu.h
@@ -0,0 +1,6 @@
+#ifndef DSMCBE_SPU_LOCAL_H_
+#define DSMCBE_SPU_LOCAL_H_
+
+#include <dsmcbe_spu.h>
+
+#endif /*DSMCBE_SPU_LOCAL_H_*/
