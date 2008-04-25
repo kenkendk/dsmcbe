676b2ae5c9bfa88987aa302679b255c770e8c026
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-04-25 15:49:12 +0000

    Cleaned up prototein code and removed warnings

--- trunk/Stage1/Prototein/SPU/spu.c
+++ trunk/Stage1/Prototein/SPU/spu.c
@@ -2,21 +2,14 @@
 #include<profile.h>
 #include<malloc.h>
 #include "DMATransfer.h"
-#include "SPUThreads.h"
 
-int DMATest(unsigned long long id);
 int FoldPrototein(unsigned long long id);
 
 int main(unsigned long long id) {
 	
-	int threadNo;
-
 	//prof_clear();
 	//prof_start();
 	
-	
-	threadNo = CreateThreads(2);
-
 	int res = FoldPrototein(id);
 	
 	//prof_stop();
