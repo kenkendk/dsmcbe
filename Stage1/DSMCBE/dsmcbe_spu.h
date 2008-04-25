2edf30d401f02a13d8b523a6d79a9b3106af9de2
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-04-25 14:03:36 +0000

    New improved structure

--- Stage1/DSMCBE/dsmcbe_spu.h
+++ Stage1/DSMCBE/dsmcbe_spu.h
@@ -1,7 +1,7 @@
 #ifndef DSMCBE_SPU_H_
 #define DSMCBE_SPU_H_
 
-#include "../Common/dsmcbe.h"
+#include "./dsmcbe.h"
 extern void initialize();
 
 #endif /*DSMCBE_SPU_H_*/
---
+++ Stage1/Testbench/guids.h
@@ -0,0 +1,6 @@
+#ifndef GUIDS_H_
+#define GUIDS_H_
+
+#define ETTAL 1
+
+#endif /*GUIDS_H_*/
