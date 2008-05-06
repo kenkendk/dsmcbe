53a6b81cf01e2a72425c792afa4bbc1a52a16cd7
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-05-06 08:12:02 +0000

    Added Stage2, based on Stage1

---
+++ trunk/Stage2/Testbench/PPU/ppu.c
@@ -0,0 +1,69 @@
+#include <dsmcbe_ppu.h>
+#include <stdio.h>
+#include "../guids.h"
+#include <common/debug.h>
+
+#define SPU_THREADS 1
+
+int main(int argc, char **argv) {
+	
+	unsigned long size;
+	unsigned int items;
+	size_t i;
+
+	printf(WHERESTR "Starting\n", WHEREARG);
+
+	pthread_t* spu_threads;
+	spu_threads = simpleInitialize(SPU_THREADS);
+
+	/*printf("ppu.c: Going to sleep\n");
+	sleep(1);*/
+	
+	printf(WHERESTR "Creating\n", WHEREARG);
+	int* data = create(ETTAL, sizeof(int));
+	(*data) = 928;
+	
+	printf(WHERESTR "Data location is %i\n", WHEREARG, (unsigned int)data);
+	
+	printf(WHERESTR "Releasing\n", WHEREARG);
+	release(data);
+	
+	data = acquire(ETTAL, &size);
+	printf(WHERESTR "ppu.c: Data location is %i\n", WHEREARG, (unsigned int)data);
+	printf(WHERESTR "ppu.c: Value is %i\n", WHEREARG, *data);
+	
+	release(data);
+
+	
+	items = 16 * 1025;
+	unsigned int* largeblock = create(LARGE_ITEM, items * sizeof(unsigned int));
+
+	printf(WHERESTR "Created large block at %d\n", WHEREARG, (unsigned int)largeblock);
+	for(i = 0; i < items; i++)
+		largeblock[i] = i;
+	
+	printf(WHERESTR "Releasing large block with %d items\n", WHEREARG, items);
+	release(largeblock);
+	
+	printf(WHERESTR "Sleeping\n", WHEREARG);
+	sleep(1);
+	
+	printf(WHERESTR "Re-acquire\n", WHEREARG);
+	largeblock = acquire(LARGE_ITEM, &size);
+	printf(WHERESTR "Acquired large block at %d (%d)\n", WHEREARG, (unsigned int)largeblock, size);
+	items = size / sizeof(unsigned int);
+	
+	for(i = 0; i < items; i++)
+	{
+		if (largeblock[i] != (i + 2))
+			printf(WHERESTR "Ivalid value at %d\n", WHEREARG, i);
+	}
+		
+	release(largeblock);
+	printf(WHERESTR "All done, exiting cleanly\n", WHEREARG);
+	
+	printf(WHERESTR "Released, waiting for SPU to complete\n", WHEREARG);
+	pthread_join(spu_threads[SPU_THREADS - 1], NULL);
+
+	return 0;
+}
