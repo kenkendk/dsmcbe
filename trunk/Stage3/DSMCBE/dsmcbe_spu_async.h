590ed3984aed27cc921de060beadef14f63eb7e6
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-05-09 18:58:42 +0000

    Almost merged Stage 2 and Stage 3, only needs to keep track of the type when the DMA starts

--- trunk/Stage3/DSMCBE/dsmcbe_spu_async.h
+++ trunk/Stage3/DSMCBE/dsmcbe_spu_async.h
@@ -3,7 +3,7 @@
 
 extern void* endAsync(unsigned int requestNo, unsigned long* size);
 extern unsigned int beginRelease(void* data);
-extern unsigned int beginAcquire(GUID id);
+extern unsigned int beginAcquire(GUID id, int type);
 unsigned int beginCreate(GUID id, unsigned long size);
 
 #endif /*DSMCBE_SPU_ASYNC_H_*/
