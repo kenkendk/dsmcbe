51ced719ed1bae4ad0a8a3723750f8a62588d8cd
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-09-03 16:56:20 +0000

    HeatEquation now runs in Stage5, but there seems to be a problem with the termination.

--- Stage5/HeatEquation/SPU/spu.c
+++ Stage5/HeatEquation/SPU/spu.c
@@ -31,25 +31,30 @@ void dsmcbe_barrier(GUID id, unsigned int ref_count)
 // Recieve last or/and first row from other(s) process	
 void ExchangeRows(PROBLEM_DATA_TYPE* data, unsigned int height, unsigned int width, PROBLEM_DATA_TYPE** firstline, PROBLEM_DATA_TYPE** lastline, unsigned int spu_no, unsigned int spu_count)
 {
+//#define memcpy(a,b,c) /* a b c */
+
 	unsigned long size;
-	void* tmp;
-	
+	void* tmp1;
+	void* tmp2;
+	unsigned int bytewidth = sizeof(PROBLEM_DATA_TYPE) * width;
+
+
 	/******* STEP 1: Release own copies *******/
 	
 	//Everyone but the first sends their updated first shared row 
     if (spu_no != 0)
     {
 		//printf(WHERESTR "SPU %d is releasing firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no);
+		memcpy(*firstline, &data[width], bytewidth);
     	release(*firstline);
-        //MPI_Isend(&data[1 * MAP_WIDTH], MAP_WIDTH, MPI_DOUBLE, mpi_rank - 1, 0, MPI_COMM_WORLD, &request_1);
     }
     
     //Everyone but the last, sends their second to last row 
     if (spu_no != spu_count - 1)
     {
 		//printf(WHERESTR "SPU %d is releasing lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no);
+		memcpy(*lastline, &data[(height-2) * width], bytewidth);
     	release(*lastline);
-        //MPI_Isend(&data[((*work_item).heigth - 2) * MAP_WIDTH], MAP_WIDTH, MPI_DOUBLE, mpi_rank + 1, 0, MPI_COMM_WORLD, &request_2);
     } 
 
 	//printf(WHERESTR "SPU %d is waiting for barrier (%d)\n", WHEREARG, spu_no, EX_BARRIER_2);
@@ -61,20 +66,23 @@ void ExchangeRows(PROBLEM_DATA_TYPE* data, unsigned int height, unsigned int wid
     if (spu_no != spu_count - 1)
     {
 		//printf(WHERESTR "SPU %d is reading firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no + 1);
-    	tmp = acquire(FIRST_ROW_OFFSET + spu_no + 1, &size, ACQUIRE_MODE_WRITE);
-    	//memcpy(&data[(height - 1) * width], tmp, size); 
-    	release(tmp);
-		//printf(WHERESTR "SPU %d has updated firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no + 1);
+    	if ((tmp1 = acquire(FIRST_ROW_OFFSET + spu_no + 1, &size, ACQUIRE_MODE_WRITE)) == NULL)
+    		printf(WHERESTR "SPU %d failed reading firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no + 1);
+    	
+    	memcpy(&data[(height - 1) * width], tmp1, bytewidth); 
+    	release(tmp1);
+		//printf(WHERESTR "SPU %d has updated firstline (%d, %ld)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no + 1, bytewidth);
     }
     
     //Everyone but the first, reads their updated first row
     if (spu_no != 0)
     {
 		//printf(WHERESTR "SPU %d is reading lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no - 1);
-    	tmp = acquire(LAST_ROW_OFFSET + spu_no - 1, &size, ACQUIRE_MODE_WRITE);
-    	//memcpy(data, tmp, size);
-    	release(tmp);
-		//printf(WHERESTR "SPU %d has updated lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no - 1);
+    	if ((tmp2 = acquire(LAST_ROW_OFFSET + spu_no - 1, &size, ACQUIRE_MODE_WRITE)) == NULL)
+			printf(WHERESTR "SPU %d failed reading lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no - 1);
+    	memcpy(data, tmp2, bytewidth);
+    	release(tmp2);
+		//printf(WHERESTR "SPU %d has updated lastline (%d, %ld)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no - 1, bytewidth);
     }
     
 	//printf(WHERESTR "SPU %d is waiting for barrier (%d)\n", WHEREARG, spu_no, EX_BARRIER_2);
@@ -82,21 +90,29 @@ void ExchangeRows(PROBLEM_DATA_TYPE* data, unsigned int height, unsigned int wid
     dsmcbe_barrier(EX_BARRIER_3, spu_count);
 	//printf(WHERESTR "SPU %d is out of barrier (%d)\n", WHEREARG, spu_no, EX_BARRIER_2);
     
+    //TODO: These two can be "beginAcquire"
     if (spu_no != spu_count - 1)
     {
 		//printf(WHERESTR "SPU %d is trying to re-acquire lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no);
     	*lastline = acquire(LAST_ROW_OFFSET + spu_no, &size, ACQUIRE_MODE_WRITE);
-        //MPI_Recv(&data[((*work_item).heigth - 1) * MAP_WIDTH], MAP_WIDTH, MPI_DOUBLE, mpi_rank + 1, 0, MPI_COMM_WORLD, &status); 
-		//printf(WHERESTR "SPU %d re-acquired lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no);
+    	if (*lastline == NULL)
+			printf(WHERESTR "SPU %d failed to re-acquire lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no);
+    	
+    	//memcpy(&data[(height - 1) * width], *lastline, bytewidth); 
+		//printf(WHERESTR "SPU %d re-acquired lastline (%d, %ld)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no, bytewidth);
     }
     
     if (spu_no != 0)
     {
 		//printf(WHERESTR "SPU %d is trying to re-acquire firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no);
     	*firstline = acquire(FIRST_ROW_OFFSET + spu_no, &size, ACQUIRE_MODE_WRITE);
-        //MPI_Recv(data, MAP_WIDTH, MPI_DOUBLE, mpi_rank - 1, 0, MPI_COMM_WORLD, &status);
-		//printf(WHERESTR "SPU %d re-acquired firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no);
+    	if (*firstline == NULL)
+			printf(WHERESTR "SPU %d failed to re-acquire firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no);
+    	
+        //memcpy(data, *firstline, bytewidth);
+		//printf(WHERESTR "SPU %d re-acquired firstline (%d, %ld)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no, bytewidth);
     }
+//#undef memcpy
 }
 
 void solve(struct Work_Unit* work_item, unsigned int spu_no, unsigned int spu_count)
@@ -114,37 +130,38 @@ void solve(struct Work_Unit* work_item, unsigned int spu_no, unsigned int spu_co
 	struct Barrier_Unit* barrier;
 	unsigned long size;
 	
-	map_width = work_item->width + 1;
+	map_width = work_item->width;
 	rc = 0;
 	// Don't update last row and last column
-	height = work_item->heigth - 1;
-	width = work_item->width - 1;
+	height = work_item->heigth ;
+	width = work_item->width;
     //data = (*work_item).problem;
     
-    printf(WHERESTR "SPU %d is creating lines\n", WHEREARG, spu_no);
+    //printf(WHERESTR "SPU %d is creating lines\n", WHEREARG, spu_no);
+	data = (PROBLEM_DATA_TYPE*)&work_item->problem;
     
     if (spu_no != 0)
     {
-    	firstline = create(FIRST_ROW_OFFSET + spu_no, sizeof(PROBLEM_DATA_TYPE) * (width + 1));
+    	firstline = create(FIRST_ROW_OFFSET + spu_no, sizeof(PROBLEM_DATA_TYPE) * width);
 	    //printf(WHERESTR "SPU %d created firstline (%d)\n", WHEREARG, spu_no, FIRST_ROW_OFFSET + spu_no);
+	    memcpy(firstline, data, sizeof(PROBLEM_DATA_TYPE) * width);
     }
     else
     	firstline = NULL;
     	
     if (spu_no != spu_count - 1)
     {
-    	lastline = create(LAST_ROW_OFFSET + spu_no, sizeof(PROBLEM_DATA_TYPE) * (width + 1));
+    	lastline = create(LAST_ROW_OFFSET + spu_no, sizeof(PROBLEM_DATA_TYPE) * width );
 	    //printf(WHERESTR "SPU %d created lastline (%d)\n", WHEREARG, spu_no, LAST_ROW_OFFSET + spu_no);
+	    memcpy(lastline, &data[width * (height - 1)], sizeof(PROBLEM_DATA_TYPE) * width);
     }
     else
     	lastline = NULL;
-    
-    printf(WHERESTR "SPU %d has created lines\n", WHEREARG, spu_no);
-    
-	data = (PROBLEM_DATA_TYPE*)&work_item->problem;
+    	
+    //printf(WHERESTR "SPU %d has created lines (%d:%d)", WHEREARG, spu_no, width, height);
 
-    delta = (*work_item).epsilon + 1.0;
-    epsilon = (*work_item).epsilon;
+    delta = work_item->epsilon + 1.0;
+    epsilon = work_item->epsilon;
     cnt = UPDATE_FREQ - 1;
     deltasum = 0.0;
 
@@ -154,8 +171,8 @@ void solve(struct Work_Unit* work_item, unsigned int spu_no, unsigned int spu_co
 
 		//printf(WHERESTR "SPU %d is solving red values\n", WHEREARG, spu_no);
 		// Update red values
-		for(y = 1; y < height; y++)
-			for (x = (y%2) + 1; x < width; x=x+2)
+		for(y = 1; y < height - 1; y++)
+			for (x = (y%2) + 1; x < width - 1; x=x+2)
 			{
 				old = MAPVALUE(x,y);
 				MAPVALUE(x,y) = 0.2 * (
@@ -166,17 +183,16 @@ void solve(struct Work_Unit* work_item, unsigned int spu_no, unsigned int spu_co
 				    MAPVALUE(x+1,y));
 				delta += ABS(old-MAPVALUE(x,y));
 				rc++;
-			}	
+			}
 
-		//printf(WHERESTR "SPU %d is done with red values\n", WHEREARG, spu_no);
-        
-        ExchangeRows(data, height + 1, width + 1, &firstline, &lastline, spu_no, spu_count);
+		//printf(WHERESTR "SPU %d is done with red values, delta: %lf\n", WHEREARG, spu_no, delta);
+       	ExchangeRows(data, height, width, &firstline, &lastline, spu_no, spu_count);
 
 		//printf(WHERESTR "SPU %d is solving black values\n", WHEREARG, spu_no);
 		
 		// Update black values
-		for(y = 1; y < height; y++)
-			for (x = 2 - (y%2); x < width; x=x+2)
+		for(y = 1; y < height - 1; y++)
+			for (x = 2 - (y%2); x < width - 1; x=x+2)
 			{
 				old = MAPVALUE(x,y);
 				MAPVALUE(x,y) = 0.2 * (
@@ -189,34 +205,54 @@ void solve(struct Work_Unit* work_item, unsigned int spu_no, unsigned int spu_co
 				rc++;
 			}	
 
-		//printf(WHERESTR "SPU %d is done with black values\n", WHEREARG, spu_no);
+		//printf(WHERESTR "SPU %d is done with black values, %lf\n", WHEREARG, spu_no, delta);
 
-        ExchangeRows(data, height + 1, width + 1, &firstline, &lastline, spu_no, spu_count);
-
-		//printf(WHERESTR "SPU %d is waiting a barrier\n", WHEREARG, spu_no);
+        ExchangeRows(data, height, width, &firstline, &lastline, spu_no, spu_count);
+		//printf(WHERESTR "SPU %d is waiting for barrier, delta: %lf\n", WHEREARG, spu_no, delta);
 
 		barrier = acquire(BARRIER_LOCK, &size, ACQUIRE_MODE_WRITE);
-		barrier->delta += delta;
+		if(barrier->lock_count == 0)
+			barrier->delta = delta;
+		else
+			barrier->delta += delta;
 		barrier->lock_count++;
+
+		//printf(WHERESTR "SPU %d is waiting for barrier, lock count: %d\n", WHEREARG, spu_no, barrier->lock_count);
+		
+#ifdef GRAPHICS
+		release(work_item);
+#endif		
 		
 		if (barrier->lock_count == spu_count + EXTRA_WAIT)
+		{
 			barrier->lock_count = 0;
+			barrier->print_count++;
+			
+			if (barrier->print_count % 100 == 0)
+				printf(WHERESTR "SPU %d is exiting barrier, delta: %lf\n", WHEREARG, spu_no, barrier->delta);
+		}
 		
 		while(barrier->lock_count != 0)
 		{
 			release(barrier);
 			barrier = acquire(BARRIER_LOCK, &size, ACQUIRE_MODE_READ);
 		}
+
+		//printf(WHERESTR "SPU %d is exiting barrier, lock count: %d\n", WHEREARG, spu_no, barrier->lock_count);
 		
-		release(barrier);
-        
 		delta = barrier->delta;
         deltasum += barrier->delta;
-		printf(WHERESTR "SPU %d is out of the barrier, deltasum: %lf, delta: %lf \n", WHEREARG, spu_no, deltasum, barrier->delta);
+		release(barrier);
+
+#ifdef GRAPHICS
+		work_item = acquire(WORK_OFFSET + spu_no, &size, ACQUIRE_MODE_WRITE);
+#endif
+        
+		//printf(WHERESTR "SPU %d is out of the barrier (%d), deltasum: %lf, delta: %lf, epsilon: %lf \n", WHEREARG, spu_no, spu_count + EXTRA_WAIT, deltasum, barrier->delta, epsilon);
 
 	}
 	
-	printf(WHERESTR "SPU %d is done solving\n", WHEREARG, spu_no);
+	//printf(WHERESTR "SPU %d is done solving\n", WHEREARG, spu_no);
 	
 	if (firstline != NULL)
 		release(firstline);
@@ -237,7 +273,7 @@ int main(long long id)
     
     initialize();
     
-	printf(WHERESTR "SPU %d is booting\n", WHEREARG, spu_no);
+	//printf(WHERESTR "SPU %d is booting\n", WHEREARG, spu_no);
 
 	struct Assignment_Unit* boot = acquire(ASSIGNMENT_LOCK, &size, ACQUIRE_MODE_WRITE);
 	spu_no = boot->spu_no;
@@ -245,14 +281,15 @@ int main(long long id)
 	boot->spu_no++;
 	release(boot);
 	
-	printf(WHERESTR "SPU %d is starting\n", WHEREARG, spu_no);
+	//printf(WHERESTR "SPU %d is starting\n", WHEREARG, spu_no);
 	    
     work_item = acquire(WORK_OFFSET + spu_no, &size, ACQUIRE_MODE_WRITE);
 
-	printf(WHERESTR "SPU %d is solving\n", WHEREARG, spu_no);
+	//printf(WHERESTR "SPU %d is solving\n", WHEREARG, spu_no);
 	solve(work_item, spu_no, spu_count);
-	printf(WHERESTR "SPU %d is finished solving\n", WHEREARG, spu_no);
+	//printf(WHERESTR "SPU %d is finished solving\n", WHEREARG, spu_no);
 	
+	//TODO: This is not the correct item if we have GRAPHICS on 
 	release(work_item);
 
 	terminate();	
