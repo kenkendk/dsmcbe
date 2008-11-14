523962c38d86ab31a6ccb69dbe49b9ed14f26803
mortenmie <mortenmie@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-11-14 13:34:59 +0000

    Added HelloWorld example.

---
+++ trunk/Applications/HelloWorld/SPU/spu.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <spu_mfcio.h>
+#include <stdio.h>
+#include <math.h>
+#include <libmisc.h>
+#include <dsmcbe_spu.h>
+
+#include "../PPU/guids.h"
+
+int main(unsigned long long speid, unsigned long long argp, unsigned long long envp)
+{
+	initialize();
+	unsigned long size;
+	printf("SPE %llu saying Hallo World\n", speid);
+		
+	unsigned int* count = acquire(COUNT, &size, ACQUIRE_MODE_WRITE);	
+	*count = *count - 1;
+	printf("SPE %llu saying COUNT is %u\n", speid, *count);
+	release(count);
+	
+	while(1)
+	{
+		count = acquire(COUNT, &size, ACQUIRE_MODE_READ);
+		if (*count == 0)
+		{
+			release(count);
+			break;			
+		}
+		release(count);
+	}
+	
+	printf("SPE %llu saying Goodbye\n", speid);
+	terminate();
+	return 0;
+}
