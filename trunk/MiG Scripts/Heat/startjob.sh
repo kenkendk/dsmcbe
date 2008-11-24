33f18a9f4ad991558898816059becfbbb25df21a
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2008-11-24 08:31:25 +0000

    New results from Cellbuzz.

--- trunk/MiG Scripts/Heat/startjob.sh
+++ trunk/MiG Scripts/Heat/startjob.sh
@@ -7,6 +7,10 @@
 #cat network.txt
 #echo "Test"
 
+
+#If we get the output from the run, we can see how much memory there really is
+free -k
+
 echo "Running ./$1 $2 network.txt $3"
 ./$1 $2 network.txt $3
 echo "Done running"
