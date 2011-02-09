c44b22a554337ca40447c799fa86132993f60892
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2011-02-09 12:26:58 +0000

    Added support for SPU threads in kNN

--- Applications/kNN/PPU/kNN-PPC.c
+++ Applications/kNN/PPU/kNN-PPC.c
@@ -37,28 +37,35 @@ int generate_random_block(void** data, unsigned int d, unsigned int n)
 	return CSP_CALL_SUCCESS;
 }
 
-int Find_kNN(unsigned int spu_count, unsigned int k, unsigned int d, unsigned int n)
+int Find_kNN(unsigned int spu_count, unsigned int spu_fibers, unsigned int k, unsigned int d, unsigned int n)
 {
 	void* result;
 	size_t total_size, element_size, elements_pr_block, blocks, buffer_required, i;
 	struct setup_item* setup;
 	void* data;
 	pthread_t* spu_threads;
+	unsigned int execution_units;
+	unsigned int problem_size;
 
 	//Initialize random seed
 	srand(time(NULL));
 
+	problem_size = DESIRED_PROBLEM_SIZE / spu_fibers;
 	element_size = sizeof(PROBLEM_TYPE) * d;
 	total_size = element_size * n;
-	elements_pr_block = DESIRED_PROBLEM_SIZE / element_size;
-	blocks = (total_size + (DESIRED_PROBLEM_SIZE - 1)) / DESIRED_PROBLEM_SIZE;
+	elements_pr_block = problem_size / element_size;
+	blocks = (total_size + (problem_size - 1)) / problem_size;
 
-	if (spu_count > blocks)
-		spu_count = blocks;
+	execution_units = spu_count * spu_fibers;
 
-	printf("Finding %d nearest neighbors in %d elements with %d dimensions, running %d blocks on %d SPUs\n", k, n, d, blocks, spu_count);
+	if (execution_units > blocks) {
+		fprintf(stderr, "There were %u SPUs with %u fibers, giving %u execution units, but only %u blocks\n", spu_count, spu_fibers, blocks);
+		exit(-1);
+	}
+
+	printf("Finding %d nearest neighbors in %d elements with %d dimensions, running %d blocks on %d:%d SPUs\n", k, n, d, blocks, spu_count, spu_fibers);
 	printf("Element size %d, total_size %d, elements_pr_block %d, blocks %d\n", element_size, total_size, elements_pr_block, blocks);
-	spu_threads = dsmcbe_simpleInitialize(0, NULL, spu_count, 1);
+	spu_threads = dsmcbe_simpleInitialize(0, NULL, spu_count, spu_fibers);
 
 	buffer_required = MAX((blocks - spu_count) + 1, 1);
 	CSP_SAFE_CALL("create setup channel", dsmcbe_csp_channel_create(SETUP_CHANNEL, buffer_required, CSP_CHANNEL_TYPE_ONE2ANY));
@@ -86,7 +93,7 @@ int Find_kNN(unsigned int spu_count, unsigned int k, unsigned int d, unsigned in
 		setup->processid = i;
 		setup->blockcount = blocks;
 		setup->items_pr_block = elements_pr_block;
-		setup->processes = spu_count;
+		setup->processes = execution_units;
 		setup->dimensions = d;
 		setup->k = k;
 
