de33113eacebc7cf7bf4c6135a18ed0c987feb66
kenneth.skovhede <kenneth.skovhede@135d68d0-7d49-0410-beab-fb2cda88b53c>
2011-02-09 12:26:58 +0000

    Added support for SPU threads in kNN

--- branches/CSPProcesses/Applications/kNN/PPU/ppu.c
+++ branches/CSPProcesses/Applications/kNN/PPU/ppu.c
@@ -4,33 +4,36 @@
 #include "StopWatch.h"
 #include <unistd.h>
 
-int Find_kNN(int spu_count, unsigned int k, unsigned int d, unsigned int n);
+int Find_kNN(int spu_count, int spu_fibers, unsigned int k, unsigned int d, unsigned int n);
 
 int main(int argc,char** argv) {
 	char buf[256];
 	int spu_threads;
+	int spu_fibers;
 	
 	int k;
 	int d;
 	int n;
 
-	if(argc == 5) {
+	if(argc == 6) {
 		spu_threads = atoi(argv[1]);		 		
-		k = atoi(argv[2]);
-		d = atoi(argv[3]);
-		n = atoi(argv[4]);
-	} else if (argc == 2) {
+		spu_fibers = atoi(argv[2]);
+		k = atoi(argv[3]);
+		d = atoi(argv[4]);
+		n = atoi(argv[5]);
+	} else if (argc == 3) {
 		spu_threads = atoi(argv[1]);
+		spu_fibers = atoi(argv[2]);
 		k = 10;
 		d = 72;
 		n = 1000;
 	} else {
-		printf("Wrong number of arguments \"./PPU spu-threads\"\n");
-		printf("                          \"./PPU spu-threads k dimensions elements\"\n");
+		printf("Wrong number of arguments \"./PPU spu-threads spu-fibers\"\n");
+		printf("                          \"./PPU spu-threads spu-fibers k dimensions elements\"\n");
 		return -1;
 	}
 	
-	if (spu_threads <= 0)
+	if (spu_threads <= 0 || spu_fibers <= 0)
 	{
 		perror("There must be at least one SPU process\n");
 		exit(-1);
@@ -45,7 +48,7 @@ int main(int argc,char** argv) {
 	sw_init();
 	sw_start();
 
-	Find_kNN(spu_threads, k, d, n);
+	Find_kNN(spu_threads, spu_fibers, k, d, n);
 	
 	sw_stop();
 	sw_timeString(buf);
